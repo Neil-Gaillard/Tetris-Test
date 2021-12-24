82c7d9bb4c75954752880c8f4ee21c1f313956f2
Neil <neil.gaillard@hotmail.com>
2021-12-24 12:26:30 +0100

    Moved window update in another method

--- Tetris-Test/src/main.cpp
+++ Tetris-Test/src/main.cpp
@@ -14,6 +14,8 @@
 #define VERTEX_SHADER_PATH "src/graphics/shaders/basic.vert"
 #define FRAGMENT_SHADER_PATH "src/graphics/shaders/basic.frag"
 
+void updateWindow(const board::Board* board, graphics::Window* window);
+
 int main(int argc, char* argv)
 {
 	srand((unsigned)time(0));
@@ -35,21 +37,25 @@ int main(int argc, char* argv)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
-	while (!window.closed()) {
-		window.clear();
-		GLfloat vertices[20 * 10][18];
-		int k = 0;
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (board.getBlockComponant(j, i)->isActive()) {
-					memcpy(vertices[k], board.getBlockComponant(j, i)->getVertices(), 18 * sizeof(GLfloat));
-					glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[k]), vertices[k], GL_STATIC_DRAW);
-					++k;
-					glDrawArrays(GL_TRIANGLES, 0, 6);
-				}
+	while (!window.closed()) 
+		updateWindow(&board, &window);
+	return 0;
+}
+
+void updateWindow(const board::Board* board, graphics::Window* window)
+{
+	window->clear();
+	GLfloat vertices[20 * 10][18];
+	int k = 0;
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (board->getBlockComponant(j, i)->isActive()) {
+				memcpy(vertices[k], board->getBlockComponant(j, i)->getVertices(), 18 * sizeof(GLfloat));
+				glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[k]), vertices[k], GL_STATIC_DRAW);
+				++k;
+				glDrawArrays(GL_TRIANGLES, 0, 6);
 			}
 		}
-		window.update();
 	}
-	return 0;
+	window->update();
 }
